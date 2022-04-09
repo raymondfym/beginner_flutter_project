9fc7cd65fc13473f5db775e538ad2966654be768
raymondfy14 <raymondfarazmalau@gmail.com>
2022-04-09 14:18:20 +0700

    Update Codelab 2
    Membuat file dart baru untuk class detail_screen dan menambah beberapa fitur, seperti gambar dan font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,62 +17,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DetailScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              child: Text(
-                'Farm House Lembang',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.bold,
-                ),)),
-            Container(
-              margin: EdgeInsets.symmetric(vertical: 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.calendar_today),
-                      SizedBox(height: 8.0),
-                      Text('Open Everyday')
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.access_time),
-                      SizedBox(height: 8.0),
-                      Text('Open Everyday')
-                    ],
-                  ),
-                  Column(
-                    children: <Widget>[
-                      Icon(Icons.monetization_on),
-                      SizedBox(height: 8.0),
-                      Text('Open Everyday')
-                    ],
-                  )
-                ],
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(16.0),
-              child: Text(
-                'Berada di jalur utama Bandung-Lembang, Farm House menjadi objek wisata yang tidak pernah sepi pengunjung. Selain karena letaknya strategis, kawasan ini juga menghadirkan nuansa wisata khas Eropa. Semua itu diterapkan dalam bentuk spot swafoto Instagramable.',
-                textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 16.0),
-              ),
-            )
-          ],
-          ),
-      ),
-    );
-  }
-}
